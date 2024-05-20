ce44889127b3dc2c98306826fdc52b50206c3a79
Vinicius J P Silva <viniciusjps15@gmail.com>
2024-05-20 15:13:24 -0300

    Creation and configuration
    + Maven project created
    + Added dependencies (pom.xml)

---
+++ src/main/java/br/vjps/sps/ServicesProvidedSystemApplication.java
@@ -0,0 +1,18 @@
+package br.vjps.sps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesProvidedSystemApplication {
+
+	/**
+	 * Starts the application.
+	 * 			
+	 * @param args main arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesProvidedSystemApplication.class, args);
+	}
+	
+}
